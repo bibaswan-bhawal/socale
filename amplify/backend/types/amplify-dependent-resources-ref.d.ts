db385fd1be2d2856ab1208a8b7882ba7a7df7cc4
Aamish Ahmad Beg <AcnoSaga@Akhlaks-MacBook-Pro.local>
2022-07-14 11:13:43 +0530

    AWS GraphQL API setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "socale": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
