b553db81a2c028f473fff3bfaaf4c62919a38794
Aamish Ahmad Beg <AcnoSaga@Akhlaks-MacBook-Pro.local>
2022-09-07 20:47:25 -0400

    User update functionality made

--- lib/services/update_service.dart
+++ lib/services/update_service.dart
@@ -3,9 +3,10 @@ import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:socale/models/User.dart';
 
 class UpdateService {
-  Future<void> updateTodo(User newUser) async {
+  Future<bool> updateUser(User newUser) async {
     final request = ModelMutations.update(newUser);
     final response = await Amplify.API.mutate(request: request).response;
+    return response.errors.isEmpty;
   }
 }
 
